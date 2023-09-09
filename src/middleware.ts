051ccccaeb66dfd6f16364fc3fba55d591f842e1
Wil Macedo <wil.macedo.sa@gmail.com>
2023-09-09 02:09:48 -0300

    feat: :sparkles: change to new mdidleware strategy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,55 +1,23 @@
-import { AUTH_DATA_KEY } from '@/constants';
-import { RequestData } from '@/services/auth';
 import { NextRequest, NextResponse } from 'next/server';
+import { AUTH_DATA_KEY, redirectUrl } from './config/auth';
+import { AuthenticateUser } from './types/requests';
 
-const authValidate = (cookie: string | undefined) => {
-  if (!cookie) return false;
-
-  try {
-    const data: RequestData = JSON.parse(cookie);
-    if (!data.token) return false;
-
-    return true;
-  } catch (e) {
-    return false;
+export default function middleware({ cookies, url }: NextRequest) {
+  const cookie = cookies.get(AUTH_DATA_KEY);
+  if (!cookie || !cookie.value) {
+    return NextResponse.redirect(new URL(redirectUrl, url));
   }
-};
-
-export default function middleware({ cookies, nextUrl, url }: NextRequest) {
-  const response = NextResponse.redirect(new URL('/auth/login', url));
-
-  const cookie = cookies.get(AUTH_DATA_KEY)?.value;
-  if (authValidate(cookie)) return NextResponse.next();
-
-  const urlParams = nextUrl.searchParams.get('data');
-  if (!urlParams) return response;
 
   try {
-    const data: RequestData = JSON.parse(urlParams);
-    if (!data.token) return response;
+    const token = JSON.parse(cookie.value) as AuthenticateUser;
+    if (!token.token) throw new Error();
 
-    let options = {};
-    const remember = nextUrl.searchParams.get('remember');
-    if (remember === 'true') {
-      options = {
-        maxAge: 60 * 60 * 24 * 30, // 30 days
-      };
-    }
-
-    const redirect = NextResponse.redirect(new URL('/', url));
-
-    redirect.cookies.set({
-      name: AUTH_DATA_KEY,
-      value: JSON.stringify(data),
-      ...options,
-    });
-
-    return redirect;
-  } catch (e) {
-    return NextResponse.redirect(new URL('/auth/login', url));
+    return NextResponse.next();
+  } catch (error) {
+    return NextResponse.redirect(new URL(redirectUrl, url));
   }
 }
 
 export const config = {
-  matcher: ['/', '/dashboard', '/settings/:path*'],
+  matcher: ['/', '/dashboard', '/settings/:path*', '/module'],
 };
